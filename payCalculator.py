1cb5f0d70fafcaba12084e79599ec09430236267
Ashok Bagadiya <Ashok.Bagadiya@vu.com>
2022-01-21 09:24:23 -0600

    added the codd

--- payCalculator.py
+++ payCalculator.py
@@ -3,6 +3,9 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
+    rate = input ("Enter Rate: ")
+    gpay = hrs*rate
+    print("Gross Pay = " + gpay)
     
     # end assignment
 
